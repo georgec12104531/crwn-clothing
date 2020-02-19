725688964aa444bda479081317e0e036901d41e0
George <georgecharleschang@gmail.com>
2020-02-18 16:03:55 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Homepage from './homepage.component';
+import './homepage.styles.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage></Homepage>
+    </div>
+  );
+}
+
+export default App;
